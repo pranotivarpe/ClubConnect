f8b0c6bfe44910601f70b771e112dcac2e2c01d3
My Name <varpe@gmail.com>
2024-10-13 19:26:43 +0530

    first commit

---
+++ js/App.js
@@ -0,0 +1,4 @@
+// Handle Admin Login button click
+document.getElementById('adminLoginBtn').addEventListener('click', function () {
+    window.location.href = 'admin-login.html';  // Redirect to Admin Login page
+});
